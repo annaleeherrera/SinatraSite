740d9c68afa55313fefd8b957b84c68caf9a2e8f
Annalee <annaleeherrera@gmail.com>
2015-10-30 14:02:24 -0700

    wave3

--- my-site.rb
+++ my-site.rb
@@ -7,14 +7,17 @@ class MySite < Sinatra::Base
   end
 
   get "/loveseattle.html" do
+    @footerlinks = {"Parks" => "seattleparks.html", "Food" => "/seattlefood.html"}
     erb :hello
   end
 
   get "/seattleparks.html" do
+    @footerlinks = {"Home" => "/loveseattle.html", "Food" => "/seattlefood.html"}
     erb :parks
   end
 
   get "/seattlefood.html" do
+    @footerlinks = {"Home" => "loveseattle.html", "Parks" => "seattleparks.html"}
     erb :food
   end
 end
